5f75dacfbf2d51dd181e0ff94b1fe4b66a518bae
jbtokam <jeanbTokam@github.com>
2024-02-09 12:49:15 -0500

    Async/await

---
+++ Master_Csharp/Async_Await/Task.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Master_Csharp.Async_Await
+{
+    class Program
+    {
+
+
+      
+
+
+
+        static async Task<string> GetApiResponseAsync(string url)
+        {
+
+
+
+            using (HttpClient client = new HttpClient()) { 
+            
+            HttpResponseMessage response = await client.GetAsync(url);
+
+                response.EnsureSuccessStatusCode();
+
+
+                return await response.Content.ReadAsStringAsync();
+            
+            
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+
+            string url = "https://jsonplaceholder.typicode.com/posts/";
+
+
+            string response = await GetApiResponseAsync(url);   
+
+
+            Console.WriteLine(response);
+
+        }
+    }
+}
