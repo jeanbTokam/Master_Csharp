9b024f3130746fc35fddacbfbb36a40a8c925f22
jbtokam <jeanbTokam@github.com>
2024-02-15 00:36:07 -0500

    Binary Search

---
+++ Master_Csharp/List/BinarySearchList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Master_Csharp.List
+{
+    public class BinarySearchList
+    {
+        public static void main()
+        {
+            List<int> list = new List<int> { 1, 2, 4, 5, 7, 8, 9 }; // Ensure the list is sorted
+
+            int key = 7;
+
+            int index = list.BinarySearch(key);
+
+            Action<int> action = item => { Console.WriteLine($"My item is {item}"); };
+            list.ForEach(action);
+
+            if (index >= 0)
+            {
+                Console.WriteLine($"Element {key} found at index {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The Element {key} is not found");
+            }
+        }
+    }
+}
